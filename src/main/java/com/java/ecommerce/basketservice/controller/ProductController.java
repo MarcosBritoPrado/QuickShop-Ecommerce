cf88e5fe6257f57d3f02f10637822beb654d4c5c
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-09-01 22:50:49 -0300

    feat(product): integrar client response e ajustar configuração do serviço
    - adiciona pacote client/response para tratar respostas externas
    - atualiza ProductController e ProductService para suportar nova integração
    - ajusta PlatziStoreClient para consumir corretamente os dados
    - atualiza application.yaml para refletir novas configs

--- src/main/java/com/java/ecommerce/basketservice/controller/ProductController.java
+++ src/main/java/com/java/ecommerce/basketservice/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.java.ecommerce.basketservice.controller;
 
+import com.java.ecommerce.basketservice.client.PlatziStoreClient;
+import com.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import com.java.ecommerce.basketservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -16,14 +20,13 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<Void> getAllproducts() {
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<PlatziProductResponse>> getAllproducts() {
+        return ResponseEntity.ok(productService.getAllproducts());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Void> getProductById(@PathVariable long id) {
+    public ResponseEntity<PlatziProductResponse> getProductById(@PathVariable long id) {
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(productService.getProductsById(id));
     }
 }
--- src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
@@ -1,15 +1,23 @@
 package com.java.ecommerce.basketservice.service;
 
+import com.java.ecommerce.basketservice.client.PlatziStoreClient;
+import com.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    public void getAllproducts(){
+    private final PlatziStoreClient platziStoreClient;
 
+    public List<PlatziProductResponse> getAllproducts(){
+        return platziStoreClient.getAllProducts();
     }
 
-    public void getProductsById(Long id){
-
+    public PlatziProductResponse getProductsById(Long id){
+        return platziStoreClient.getProductById(id);
     }
 }
