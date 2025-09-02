1c187c029318190e718c9c4f3f40a7e80ba27e0c
MarcosBritoPrado <marcos.vinicius05@souunit.com.br>
2025-09-01 21:44:14 -0300

    feat: ProductController e ProductService

---
+++ src/main/java/com/java/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.java.ecommerce.basketservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void getAllproducts(){
+
+    }
+
+    public void getProductsById(Long id){
+
+    }
+}
